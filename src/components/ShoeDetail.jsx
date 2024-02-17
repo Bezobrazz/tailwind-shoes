9da1063018fcfe0997361448f63eb45ebf247ae9
Viacheslav Kostenko <kostenko.uvd@gmail.com>
2024-02-17 11:02:15 +0200

    added shoe detail (hero)

---
+++ src/components/ShoeDetail.jsx
@@ -0,0 +1,32 @@
+import nike1 from "../assets/n1-min.png";
+const ShoeDetail = () => {
+  return (
+    <div className="flex flex-col lg:flex-row-reverse space-y-4">
+      <div className="flex-1 ">
+        Image
+        <img src={nike1} alt="" />
+      </div>
+      <div className="flex-1 space-y-6">
+        <h1 className="text-5xl font-black md:text-9xl">Nike Air max 270</h1>
+        <h2 className="font-medium md:text-xl">
+          The Nike Air max 270 is a lifestyle shoe that`s sure to turn heads
+          with it`s vibrant color gradient
+        </h2>
+        <p className="text-3xl font-extrabold md:text-6xl">100 $</p>
+        <div className="space-x-10">
+          <button className="h-14 w-44 bg-black text-white hover:bg-gray-900 active:bg-gray-700">
+            Add to cart
+          </button>
+          <a
+            className="text-lg font-bold underline underline-offset-4"
+            href="#"
+          >
+            View details
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ShoeDetail;
