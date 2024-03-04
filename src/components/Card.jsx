3555840a96415337f287b7f022aace44d0d47289
Viacheslav Kostenko <103434668+Bezobrazz@users.noreply.github.com>
2024-03-04 22:29:48 +0200

    added card

---
+++ src/components/Card.jsx
@@ -0,0 +1,19 @@
+const Card = ({ item }) => {
+  return (
+    <div
+      className={`${item.className} max-w-xl cursor-pointer transform transition hover:scale-105`}
+    >
+      <div className="p-8">
+        <h2 className="mb-10 text-2xl font-bold">{item.title}</h2>
+        <a className="underline underline-offset-4 font-semibold">SHOP NOW +</a>
+      </div>
+      <img
+        className="absolute top-5 left-[40%] h-40 w-26"
+        src={item.src}
+        alt={item.title}
+      />
+    </div>
+  );
+};
+
+export default Card;
