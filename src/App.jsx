5a637228c3a43da0d7efcc2feed8b8390b0eee70
Viacheslav Kostenko <103434668+Bezobrazz@users.noreply.github.com>
2024-03-25 21:02:13 +0200

    setup dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { BiMoon } from "react-icons/bi";
+import { BiSun } from "react-icons/bi";
 import Nav from "./components/Nav";
 import ShoeDetail from "./components/ShoeDetail";
 import { SHOE_LIST } from "./constants";
 import Sidebar from "./components/Sidebar";
 import NewArrivalsSection from "./components/NewArrivalsSection";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Cart from "./components/Cart.jsx";
 
 const FAKE_CART_ITEMS = SHOE_LIST.map((shoe) => {
@@ -17,6 +19,22 @@ const FAKE_CART_ITEMS = SHOE_LIST.map((shoe) => {
 export default function App() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
+  useEffect(() => {
+    const isDarkMode = localStorage.getItem("isDarkMode");
+    if (isDarkMode === "true") {
+      window.document.documentElement.classList.add("dark");
+    }
+  }, []);
+
+  const toggleDarkMode = () => {
+    window.document.documentElement.classList.toggle("dark");
+
+    localStorage.setItem(
+      "isDarkMode",
+      window.document.documentElement.classList.contains("dark")
+    );
+  };
+
   return (
     <div className="animate-fadeIn p-10 xl:px-24">
       <Nav onClickShoppingButton={() => setIsSidebarOpen(true)} />
@@ -26,12 +44,17 @@ export default function App() {
         isOpen={isSidebarOpen}
         onClickClose={() => setIsSidebarOpen(false)}
       >
-        {/* <h2 className="text-2xl font-bold mb-10">Cart</h2>
-        <CartItem item={SHOE_LIST[0]} />
-        <CartItem item={SHOE_LIST[1]} />
-        <CartItem item={SHOE_LIST[2]} /> */}
         <Cart cartItems={FAKE_CART_ITEMS} />
       </Sidebar>
+      <div className="fixed bottom-4 right-4 ">
+        <button
+          onClick={toggleDarkMode}
+          className="shadow-lg bg-night-50 px-4 py-2 rounded-full text-white dark:bg-white dark:text-night"
+        >
+          <BiSun className="hidden dark:block" />
+          <BiMoon className="dark:hidden" />{" "}
+        </button>
+      </div>
     </div>
   );
 }
