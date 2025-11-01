c8a9d0a51369fa48d8d799e5ac428e65812e1cf3
Bezobrazz <kostenko.uvd@gmail.com>
2025-11-01 13:36:38 +0200

    enhanced Cart component by adding a Checkout button and restructuring the layout for better user interaction

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -9,13 +9,18 @@ const Cart = ({ cartItems, onClickTrash }) => {
           Shopping cart is empty, add a product.
         </p>
       ) : (
-        <ul className="space-y-5">
-          {cartItems.map((cartItem) => (
-            <li key={cartItem.product.id}>
-              <CartItem item={cartItem} onClickTrash={onClickTrash} />
-            </li>
-          ))}
-        </ul>
+        <div className="flex flex-col space-y-6">
+          <ul className="space-y-5">
+            {cartItems.map((cartItem) => (
+              <li key={cartItem.product.id}>
+                <CartItem item={cartItem} onClickTrash={onClickTrash} />
+              </li>
+            ))}
+          </ul>
+          <button className=" bg-black px-5 py-3 text-sm font-semibold text-white transition hover:bg-black/80 dark:bg-white dark:text-night dark:hover:bg-white/80">
+            Checkout
+          </button>
+        </div>
       )}
     </>
   );
